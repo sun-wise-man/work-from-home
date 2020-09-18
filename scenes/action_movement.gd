a58887ea2276218ceb31ff75e402745d16d4613d
MIrfanKamal <irfan.kamal12@gmail.com>
2020-09-18 22:17:27 +0700

    Membuat movement menjadi pathfinding dan menambah element bar

--- scenes/action_movement.gd
+++ scenes/action_movement.gd
@@ -1,28 +1,41 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
+export (int) var speed = 50
 
-var target = Vector2()
-var velocity = Vector2()
 var idle = true
 
-func _input(event):
-	var direction = sign(get_global_mouse_position().x - $AnimatedSprite.global_position.x)
-	if event.is_action_pressed('click'):
-		target = get_global_mouse_position()
+# Untuk menyimpan runtutan perjalanan
+var path : = PoolVector2Array()
+
+func _process(delta):
+	# Calculate the movement distance for this frame
+	var distance_to_walk = speed * delta
+	
+	# Move the player along the path until he has run out of movement or the path ends.
+	while distance_to_walk > 0 and path.size() > 0:
+		var distance_to_next_point = position.distance_to(path[0])
+		if distance_to_walk <= distance_to_next_point:
+			# The player does not have enough movement left to get to the next point.
+			position += position.direction_to(path[0]) * distance_to_walk
+		else:
+			# The player get to the next point
+			position = path[0]
+			path.remove(0)
+			if path.size() == 0:
+				$AnimatedSprite.play("idle")
+			flip_char()
+		# Update the distance to walk
+		distance_to_walk -= distance_to_next_point
+
+func flip_char():
+	if path.size() > 0:
+		var direction = sign(path[0].x - $AnimatedSprite.global_position.x)
 		if direction < 0:
 			$AnimatedSprite.set_flip_h(true)
 		else:
 			$AnimatedSprite.set_flip_h(false)
-
-func _physics_process(delta):
-	velocity = position.direction_to(target) * speed
-	
-
-	if position.distance_to(target) > 5:
-		velocity = move_and_slide(velocity)
-		$AnimatedSprite.play("walk")
-
-	else:
-		$AnimatedSprite.play("idle")
-	
+			
+func take_path(the_path):
+	path = the_path
+	flip_char()
+	$AnimatedSprite.play("walk")
