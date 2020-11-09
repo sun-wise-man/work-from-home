10be1143a83ffc75125cc6462c4dae72adf61164
MIrfanKamal <irfan.kamal12@gmail.com>
2020-11-09 12:59:56 +0700

    Buff tooltip and continue on pause

---
+++ scenes/BuffTooltip.gd
@@ -0,0 +1,41 @@
+extends Panel
+
+
+func _process(delta):
+	if visible:
+		rect_position = get_global_mouse_position()
+
+
+func _on_HappinessBuff_mouse_entered():
+	visible = true
+	$Label.text = "Bonus money"
+
+
+func _on_HappinessBuff_mouse_exited():
+	visible = false
+
+func _on_HappinessDebuff_mouse_entered():
+	visible = true
+	$Label.text = "speed down"
+
+
+func _on_HappinessDebuff_mouse_exited():
+	visible = false
+
+
+func _on_HealthBuff_mouse_entered():
+	visible = true
+	$Label.text = "speed up"
+
+
+func _on_HealthBuff_mouse_exited():
+	visible = false
+
+
+func _on_HealthDebuff_mouse_entered():
+	visible = true
+	$Label.text = "dark screen"
+
+
+func _on_HealthDebuff_mouse_exited():
+	visible = false
